12067ed6461261d70b841a703854bbf85f206c2b
AhmedEsam2005 <medoesam409@gmail.com>
2020-12-19 13:12:49 +0200

    ....

--- js/plugin.js
+++ js/plugin.js
@@ -6,4 +6,20 @@ $(document).ready(function ()
         $(".icon").toggleClass("fa-moon-o");
         $(".icon").addClass("fa-sun-o");
     });
+    $(".landing-overlay").fadeOut(1000,function () 
+    {
+        $("body").css('overflow','auto');
+    });
+
 });
+// Start Scroll Top Button
+window.onscroll = () => {
+    var btn = document.querySelector(".scroll button");
+    btn.style.opacity = 0;
+    if (document.documentElement.scrollTop >= 200) {
+        btn.style.opacity = 1;
+    }
+    btn.onclick = () => {
+        document.documentElement.scrollTop = 0;
+    };
+};
