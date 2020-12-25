efbdd5473f9b7965352950dc10ee094337f2191b
AhmedEsam2005 <medoesam409@gmail.com>
2020-12-25 15:21:42 +0200

    ....

--- js/plugin.js
+++ js/plugin.js
@@ -19,7 +19,38 @@ window.onscroll = () => {
     if (document.documentElement.scrollTop >= 200) {
         btn.style.opacity = 1;
     }
-    btn.onclick = () => {
-        document.documentElement.scrollTop = 0;
+    btn.onclick = function (e) {
+        e.preventDefault();
+        $("body, html").animate({
+            scrollTop: 0
+        });
     };
 };
+
+// Edit In The Item 
+
+var item = document.querySelectorAll(".price");
+var allItem = Array.from(item);
+allItem.forEach(item => {
+    item.style.display = 'block';
+
+});
+console.table(allItem);
+// Start Local Storage Item
+var numItem = localStorage.length;
+window.onload = () => {
+    document.querySelector(".basket .number").textContent = numItem;
+}
+// STart Choose Mode
+$(".dark-mode").click(function () {
+
+    $("body").addClass("dark");
+    $(".choose").hide(1000);
+
+});
+$(".light-mode").click(function () {
+
+    $("body").removeClass("dark");
+    $(".choose").hide();
+
+});
